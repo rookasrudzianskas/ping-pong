10f80db1d5fe899de26bc31c792da78b5bbe7bfe
Rokas <rookas.rudzenskas@gmail.com>
2022-11-11 20:50:53 +0200

    Rokas

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-   // he are you in? are you working fine?
+   // he are you in? are you working fine? is Github Copilot going to work?????
+    // no???? not working at all, so does it mean that I have to write all the code myself?????
   return (
       <View className="items-center justify-center h-screen">
         <View style={styles.ball} className="w-5 h-5 bg-black rounded-full"/>
@@ -13,10 +14,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
   },
     ball: {
       position: 'absolute',
