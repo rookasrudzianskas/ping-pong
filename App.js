2cd9d4c1a875938e08a683d416fd4c810fd7a74e
Rokas <rookas.rudzenskas@gmail.com>
2022-11-12 10:12:26 +0200

    Add a bouncing ball
    This is a bouncing ball. It bounces off the walls.

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, useWindowDimensions, View} from 'react-native';
 import Animated, {Easing, useAnimatedStyle, useSharedValue, withTiming} from 'react-native-reanimated';
 import {useEffect} from "react";
 
 const FPS = 60;
 const DELTA = 1000 / FPS;
-const SPEED = 0.5;
+const SPEED = 6;
 
 const normalizeVector = (vector) => {
     // this is c in pythagorean theorem
@@ -17,13 +17,13 @@ const normalizeVector = (vector) => {
 }
 
 export default function App() {
-    const targetPositionX = useSharedValue(0);
-    const targetPositionY = useSharedValue(0);
+    const targetPositionX = useSharedValue(200);
+    const targetPositionY = useSharedValue(200);
+    const { height, width } = useWindowDimensions();
     const direction = useSharedValue(normalizeVector({
-        x: 100,
-        y: 150,
+        x: Math.random(),
+        y: Math.random(),
     }));
-    console.log(JSON.stringify(direction));
 
     useEffect(() => {
         const interval = setInterval(update, DELTA);
@@ -31,8 +31,21 @@ export default function App() {
     }, []);
 
     const update = () => {
-        targetPositionX.value = withTiming(targetPositionX.value + direction.value.x * SPEED, {duration: DELTA, easing: Easing.linear});
-        targetPositionY.value = withTiming(targetPositionY.value + direction.value.y * SPEED, {duration: DELTA, easing: Easing.linear});
+        const nextX = targetPositionX.value + direction.value.x * SPEED;
+        const nextY = targetPositionY.value + direction.value.y * SPEED;
+
+        if(nextY < 0 || nextY > height) {
+            console.log("WE HIT THE TOP WALL");
+            direction.value = { x: direction.value.x, y: -direction.value.y}
+        }
+
+        if(nextX < 0 || nextX > width) {
+            console.log("WE HIT THE SIDE WALL");
+            direction.value = { x: -direction.value.x, y: direction.value.y}
+        }
+
+        targetPositionX.value = withTiming(nextX, {duration: DELTA, easing: Easing.linear});
+        targetPositionY.value = withTiming(nextY, {duration: DELTA, easing: Easing.linear});
     }
 
     const ballAnimatedStyles = useAnimatedStyle(() => {
